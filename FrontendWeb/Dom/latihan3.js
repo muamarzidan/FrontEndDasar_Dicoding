9161bf15963c305e2630255c661ed033d4689043
muamarzidan <codeofomiru11@gmail.com>
2024-02-12 11:10:50 +0700

    add latihan 3

---
+++ FrontendWeb/Dom/latihan3.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const newli = document.createElement('li');
+    const listDaftar = document.getElementById('daftar');
+    
+    newli.innerText = 'Item dwdwddwd';
+    listDaftar.appendChild(newli);
+});
